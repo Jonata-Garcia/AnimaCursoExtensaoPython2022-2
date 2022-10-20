5ff7aa160317a00e228980e893c7366cf721e71f
Jonata-Garcia <116230424+Jonata-Garcia@users.noreply.github.com>
2022-10-20 01:18:23 +0000

    Aprendi e exibir de 3 maneira diferente o print() com String e variável inteira

--- main.py
+++ main.py
@@ -4,10 +4,18 @@
 print("Hello Word !!!")
 
 # Quando quiser guardar uma String
-nome = " Jonata Garcia"
+nome ="Jonata Garcia"
 #Quando quiser gaudar um número inteiro
-idade = 20
+idade =20
 
 # Exibir o meu nome ( que está dentro da variávelo nome)
 print(nome)
 print(idade)
+
+#Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade    (3 maneiras)
+print("Minha idade é " +str(idade)+ " anos")
+print(f"Minha idade é {idade} anos\n")
+print("Minha idade é {} anos\n".format(idade))
+
+# Quando quiser exibir "Meu nome é... e tenho... anos..." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
