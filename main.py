9ccc4078acfb8ce63decf3119034dba5e71d5769
Jonata-Garcia <116230424+Jonata-Garcia@users.noreply.github.com>
2022-10-20 00:53:29 +0000

    Aprendendo a programar em Python :)

---
+++ main.py
@@ -0,0 +1,13 @@
+# Meu primeiro projeto Python !!!
+#
+# print() = comando saida
+print("Hello Word !!!")
+
+# Quando quiser guardar uma String
+nome = " Jonata Garcia"
+#Quando quiser gaudar um número inteiro
+idade = 20
+
+# Exibir o meu nome ( que está dentro da variávelo nome)
+print(nome)
+print(idade)
