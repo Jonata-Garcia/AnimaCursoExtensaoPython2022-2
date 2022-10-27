ee4de007aa55dcf0c67b7d71c89c9e0541d42e13
Jonata-Garcia <116230424+Jonata-Garcia@users.noreply.github.com>
2022-10-27 01:40:54 +0000

    Aula_02 de Python

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-# Meu primeiro projeto Python !!!
-#
-# print() = comando saida
-print("Hello Word !!!")
+#exemplo de laço
+numero =0
 
-# Quando quiser guardar uma String
-nome ="Jonata Garcia"
-#Quando quiser gaudar um número inteiro
-idade =20
-
-# Exibir o meu nome ( que está dentro da variávelo nome)
-print(nome)
-print(idade)
-
-#Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade    (3 maneiras)
-print("Minha idade é " +str(idade)+ " anos")
-print(f"Minha idade é {idade} anos\n")
-print("Minha idade é {} anos\n".format(idade))
-
-# Quando quiser exibir "Meu nome é... e tenho... anos..." trocando pelas variáveis nome e idade
-print("Meu nome é {} e tenho {} anos".format(nome,idade))
+while numero <=10 :
+  
+  numero = numero +1 
+  print (numero)
+  
